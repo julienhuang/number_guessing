47339f4ac154b18a0c0d301751f1928ab8a75bc5
Jilien Huang <chiayen.huang@gmail.com>
2019-01-24 10:53:04 +0800

    update 19Jan24

--- number_guess.py
+++ number_guess.py
@@ -1,11 +1,12 @@
 #猜數字遊戲
+upper_limit = int(input('請決定猜數字的範圍要從1到哪個整數(N)呢?'))
 import random
-answer = random.randint(1,10)
+answer = random.randint(1,upper_limit)
 n = 0
 while n < 5:
 	m = 4 - n
 	s = n + 1
-	keyin = int(input('請從1~10之中猜一個數字(整數)'))
+	keyin = int(input('請從1到N之中猜一個數字(整數)'))
 	if m == 0:
 		print('很抱歉! 遊戲結束了!')
 		break
@@ -22,3 +23,4 @@ while n < 5:
 		print('剩餘次數 = ', m, '次')
 	n = n + 1
 print('正確答案是: ', answer)
+
